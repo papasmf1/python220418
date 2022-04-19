8314a339f6e6fa42b8c6389deee6a12611ddf253
papasmf1 <papasmf1@gmail.com>
2022-04-19 10:21:16 +0900

    3장 함수 완료

--- function2.py
+++ function2.py
@@ -34,6 +34,7 @@ print(connectURI(port="80",server="credu.com"))
 
 #가변인자(가변적인 상황)
 def union(*ar):
+    #유니크한 글자를 담을 지역변수(List)
     result = []
     for item in ar:
         for x in item:
@@ -44,3 +45,22 @@ def union(*ar):
 #호출
 print(union("HAM","EGG"))
 print(union("HAM","EGG","SPAM"))
+
+#정의되지 않은 인자처리(**딕셔너리)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+
+#호출
+print(userURIBuilder("credu.com", "80", id="kim", passwd="1234"))
+print(userURIBuilder("credu.com", "80", id="kim", passwd="1234", name="mike"))
+
+#람다함수(이름이 없는 간결한 함수 정의)
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+#보통은 정의하고 바로 호출
+print((lambda x:x*x)(3))
+print(globals())
