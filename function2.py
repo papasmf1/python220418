5d845c09a343e85bacc5276603198bc355766760
papasmf1 <papasmf1@gmail.com>
2022-04-19 09:48:09 +0900

    가변인자 처리

--- function2.py
+++ function2.py
@@ -14,3 +14,33 @@ def func2(a):
 #호출
 print(func2(1))
 
+#기본값 지정
+def times(a=10,b=20):
+    return a*b 
+
+#호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#키워드 인자
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port 
+    return strURL 
+
+#호출
+print(connectURI("credu.com","80"))
+print(connectURI(port="80",server="credu.com"))
+
+#가변인자(가변적인 상황)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출
+print(union("HAM","EGG"))
+print(union("HAM","EGG","SPAM"))
