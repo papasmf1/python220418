8e502e44e2c1658bae4ff4b1f9a180f25eb92e18
papasmf1 <papasmf1@gmail.com>
2022-04-19 09:21:31 +0900

    함수 디버깅 연습

--- fuction1.py
+++ fuction1.py
@@ -14,11 +14,14 @@ print(retValue)
 def intersect(prelist, postlist):
     #지역변수
     result = []
+    #H(0) | A(1) | M(2)
     for x in prelist:
+        #x라는 글자가 postlist에 있고 그리고 x result에 없으면
+        #in단독으로 사용하면 include
         if x in postlist and x not in result:
             result.append(x)
     return result 
 
-#호출
+#호출(중단점을 추가)
 print(intersect("HAM","SPAM"))
 
