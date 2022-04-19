b6b65747538e9192291a6a2717a00b0b099825a1
papasmf1 <papasmf1@gmail.com>
2022-04-19 09:11:30 +0900

    함수연습2

--- fuction1.py
+++ fuction1.py
@@ -10,4 +10,15 @@ def setValue(newValue):
 retValue = setValue(5)
 print(retValue)
 
+#교집합 문자를 리턴하는 함수
+def intersect(prelist, postlist):
+    #지역변수
+    result = []
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result 
+
+#호출
+print(intersect("HAM","SPAM"))
 
