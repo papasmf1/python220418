5ba1729d7c738221e8e50691f80dac7f9a7bc92f
papasmf1 <papasmf1@gmail.com>
2022-04-19 13:44:19 +0900

    클래스 연습3

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,20 @@
+#전역변수
 str = "Not Class Member"
+
+#파이썬은 명확하게 코딩하는 것이 좋다! 
+#클래스를 정의
 class GString:
+    #초기화 메서드
     def __init__(self):
+        #인스턴스 멤버 변수 
         self.str = "" 
+    #셋팅하는 메서드(세터)
     def set(self, msg):
         self.str = msg
+    #값을 읽는 게터
     def print(self):
-        print(str)
+        #버그 발생! 
+        print(self.str)
 
 g = GString()
 g.set("First Message")
