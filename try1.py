b77c7a0f0ff323344c1af5fb3f1a75c8c57a31cd
papasmf1 <papasmf1@gmail.com>
2022-04-20 10:10:02 +0900

    에러처리 연습2

--- try1.py
+++ try1.py
@@ -1,13 +1,21 @@
 # try1.py 
-
 #함수 정의
 def divide(a,b):
     return a/b 
 
-#함수 호출
-result = divide(5,0)
-print("결과:{0}".format(result))
-
+try:
+    #함수 호출
+    result = divide(5,0)
+except TypeError:
+    print("숫자여야 합니다.")
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다.")
+except:
+    print("예상하지 못한 에러")
+else:
+    print("결과:{0}".format(result))
+finally:
+    print("한번 더 체크(무조건 실행)")
 
 print("전체 코드 실행 종료")
 
