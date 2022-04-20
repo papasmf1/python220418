640f3c7f0692bfba1453767a7cd89e9832e96534
papasmf1 <papasmf1@gmail.com>
2022-04-20 11:44:46 +0900

    문자열 처리 메서드 연습

---
+++ demoStr.py
@@ -0,0 +1,12 @@
+# demoStr.py 
+#print(dir(str))
+
+strA = "파이썬은 무지 강력해!"
+print(len(strA))
+strB = "python is very powerful"
+print(len(strB))
+print(strB.capitalize())
+print(strB.count("p"))
+print(strB.count("p", 7))
+
+
