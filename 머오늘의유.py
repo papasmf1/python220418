565e420c7eee711210cfb90fd592c31c091c4297
papasmf1 <papasmf1@gmail.com>
2022-04-21 13:49:35 +0900

    오늘의 유머 크롤링

--- 머오늘의유.py
+++ 머오늘의유.py
@@ -7,9 +7,9 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+for n in range(1,11):
+        #오늘의 유머 웹 사이트 
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -18,16 +18,19 @@ for n in range(0,10):
         #한글이 깨지는 경우는 디코딩
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        # <span class="subject_fixed" data-role="list-title-text">
-        # 		레오폴드 FC660M PD 저소음 적축 화이트 핑크
-        # </span>
+ 
+        #<td class="subject">
+        # <a href="/board/view.php?">기름값이 걱정인 사회 초년생</a>
+        # <span class="list_memo_count_span"> 
+
         #<span>태그의 attributes속성들
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        list = soup.find_all('td', attrs={'class':'subject'})
         for item in list:
                 try:
-                        title = item.text 
-                        if (re.search('아이패드', title)):
-                                print(title.strip())
+                        title = item.find("a").text.strip() 
+                        print(title)
+                        # if (re.search('아이패드', title)):
+                        #         print(title.strip())
                 except:
                         pass
         
