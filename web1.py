e26ce4a6a62a01bf43aeb024f650ac9506a564ec
papasmf1 <papasmf1@gmail.com>
2022-04-21 11:24:46 +0900

    웹 크롤링 연습3

--- web1.py
+++ web1.py
@@ -15,5 +15,20 @@ soup = BeautifulSoup(page, "html.parser")
 #print(soup.find("p"))
 
 #필터링: <p class="outer-text">컨텐츠</p>
-print(soup.find_all("p", class_="outer-text"))
+#print(soup.find_all("p", class_="outer-text"))
+
+#특정 id를 검색
+#print(soup.find_all(id="first"))
+
+#태그 안쪽에 문자열 검색
+for item in soup.find_all("p"):
+    #Tag안쪽의 문자열만 추출(.text)
+    title = item.text 
+    title = title.replace("\n", "")
+    title = title.replace("\t", "")
+    print(title.strip())
+
+
+
+
 
