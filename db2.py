4664732de1ecd76248ccee3c9244db4644f11a88
papasmf1 <papasmf1@gmail.com>
2022-04-21 09:27:38 +0900

    데이터베이스 연습5

--- db2.py
+++ db2.py
@@ -1,9 +1,8 @@
 # db1.py 
 import sqlite3
-
 #연결객체를 만들기
 #파일에 데이터베이스를 저장
-con = sqlite3.connect("c:\\work\\test.db")
+con = sqlite3.connect("c:\\work\\test2.db")
 #구문을 수행할 커서 객체 
 cur = con.cursor() 
 #테이블 구조(테이블 스키마):SQL구문은 대소문자 구분안함. 
@@ -18,14 +17,6 @@ cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber))
 datalist = (("tom","010-123"),("dsp","010-333"))
 cur.executemany("insert into PhoneBook values (?, ?);", datalist)
 #검색
-cur.execute("select * from PhoneBook;")
-# for row in cur:
-#     print(row)
-print("---fetchone()---")
-print(cur.fetchone())
-print("---fetchmany(2)---")
-print(cur.fetchmany(2))
-print("---fetchall()---")
 cur.execute("select * from PhoneBook;")
 print(cur.fetchall())
 
