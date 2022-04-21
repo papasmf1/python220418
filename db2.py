5552f7261db70d5f8336baed860d269a24b32bc7
papasmf1 <papasmf1@gmail.com>
2022-04-21 09:31:30 +0900

    데이터베이스 완료

--- db2.py
+++ db2.py
@@ -2,7 +2,7 @@
 import sqlite3
 #연결객체를 만들기
 #파일에 데이터베이스를 저장
-con = sqlite3.connect("c:\\work\\test2.db")
+con = sqlite3.connect("c:\\work\\commit.db")
 #구문을 수행할 커서 객체 
 cur = con.cursor() 
 #테이블 구조(테이블 스키마):SQL구문은 대소문자 구분안함. 
@@ -19,6 +19,8 @@ cur.executemany("insert into PhoneBook values (?, ?);", datalist)
 #검색
 cur.execute("select * from PhoneBook;")
 print(cur.fetchall())
+#작업을 완료하고 종료
+con.commit()
 
 
 
